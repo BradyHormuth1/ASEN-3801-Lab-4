02ecfff31f2df3cea7eb95a084fe5e9ae96f95fa
meganbalducci <157171662+meganbalducci@users.noreply.github.com>
2025-11-04 08:53:40 -0700

    Create PlotAircraftSim.m

---
+++ PlotAircraftSim.m
@@ -0,0 +1,210 @@
+function PlotAircraftSim(time, aircraft_state_array, control_input_array, fig, col)
+
+% PlotAircraftSim(time, aircraft_state_array, control_input_array, fig, col)
+% time:               [n×1]
+% aircraft_state_array: [n×12] ordered as:
+%   [x y z phi theta psi uE vE wE p q r]
+% control_input_array: [4×n] rows = [Zc; Lc; Mc; Nc]
+% fig:                 vector of figure numbers (len ≥ 6)
+% col:                 line style/color for 2nd traces (e.g., 'b-')
+
+% --------- validate/shape ----------
+time = time(:);
+X = aircraft_state_array;
+if size(X,1) ~= numel(time) && size(X,2) == numel(time)
+    X = X.'; % make rows = time
+end
+assert(size(X,1) == numel(time) && size(X,2) == 12, ...
+    'aircraft_state_array must be [n×12] aligned with time');
+
+if size(control_input_array,2) ~= numel(time) && size(control_input_array,1) == numel(time)
+    control_input_array = control_input_array.'; % want [4×n]
+end
+if size(control_input_array,1) ~= 4
+    error('control_input_array must be [4×n] with rows [Zc; Lc; Mc; Nc].');
+end
+
+% Unpack the rt_etim states
+XE = X(:,1); 
+YE = X(:,2); 
+ZE = X(:,3);
+psi = X(:,4); 
+theta = X(:,5); 
+phi = X(:,6);
+uE = X(:,7); 
+vE = X(:,8); 
+wE = X(:,9);
+p = X(:,10); 
+q = X(:,11); 
+r = X(:,12);
+
+% Unpack controls from control_input_array
+Zc = control_input_array(1,:); 
+Lc = control_input_array(2,:);
+Mc = control_input_array(3,:);
+Nc = control_input_array(4,:);
+
+% ---------------- PLOTTING ----------------
+lw = 1.25; %linewidth
+% Fig 1: Inertial Position
+figure(fig(1));
+
+subplot(3,1,1); 
+plot(time,XE, col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('X_E [m]');
+axis tight;
+
+subplot(3,1,2); 
+plot(time,YE,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('Y_E [m]');
+axis tight;
+
+subplot(3,1,3); 
+plot(time,ZE, col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('Z_E [m]'); 
+axis tight;
+
+xlabel('Time [s]');
+sgtitle('Inertial Position');
+
+
+% Fig 2: Euler Angles
+figure(fig(2));
+
+subplot(3,1,1); 
+plot(time,psi, col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('\psi [rad]');
+axis tight;
+
+subplot(3,1,2); 
+plot(time,theta,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('\theta [rad]');
+axis tight;
+
+subplot(3,1,3); 
+plot(time,phi,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('\phi [rad]'); 
+axis tight;
+
+xlabel('Time [s]');
+sgtitle('Euler Angles');
+
+
+% Fig 3: Inertial Velocity
+figure(fig(3));
+
+subplot(3,1,1); 
+plot(time,uE,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('u_E [m/s]');
+axis tight;
+
+subplot(3,1,2); 
+plot(time,vE,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('v_E [m/s]');
+axis tight;
+
+subplot(3,1,3); 
+plot(time,wE,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('w_E [m/s]'); 
+axis tight;
+
+xlabel('Time [s]');
+sgtitle('Inertial Velocity');
+
+% Fig 4: Angular Velocity
+figure(fig(4));
+
+subplot(3,1,1); 
+plot(time,p,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('p [rad/s]');
+axis tight;
+
+subplot(3,1,2); 
+plot(time,q,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('q [rad/s]');
+axis tight;
+
+subplot(3,1,3); 
+plot(time,r,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('r [rad/s]'); 
+axis tight;
+
+xlabel('Time [s]');
+sgtitle('Angular Velocity');
+
+
+% Fig 5: Control Inputs
+figure(fig(5)); 
+tiledlayout(4,1);
+
+nexttile; 
+plot(time,Zc,col,'LineWidth',lw); 
+grid on;
+hold on; 
+ylabel('Z_c');
+axis tight;
+
+nexttile; 
+plot(time,Lc,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('L_c');
+axis tight;
+
+nexttile; 
+plot(time,Mc,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('M_c');
+axis tight;
+
+nexttile; 
+plot(time,Nc,col,'LineWidth',lw); 
+grid on; 
+hold on; 
+ylabel('N_c'); 
+axis tight;
+
+xlabel('Time [s]');
+sgtitle('Control Inputs');
+
+% Fig 6: 3D Trajectory
+figure(fig(6)); 
+hold on; 
+grid on; 
+axis equal
+Zup = -ZE; % flip sign if NED convention
+plot3(XE, YE, Zup, 'w', 'LineWidth', lw);
+plot3(XE(1), YE(1), Zup(1), 'go', 'MarkerSize', 7);
+plot3(XE(end), YE(end), Zup(end), 'rx', 'MarkerSize', 7);
+xlabel('X_E [m]'); 
+ylabel('Y_E [m]'); 
+zlabel('Z_{up} [m]');
+title('3D Trajectory: Start=Green, End=Red');
+view(3);
+
+end
