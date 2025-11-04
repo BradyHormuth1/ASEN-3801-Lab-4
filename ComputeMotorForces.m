1e614cb59d54cc5d561c009223a08881d00c355e
BradyHormuth1 <brho8099@colorado.edu>
2025-11-04 09:43:00 -0700

    Add files via upload

--- ComputeMotorForces.m
+++ ComputeMotorForces.m
@@ -1,8 +1,17 @@
 function motor_forces = ComputeMotorForces(Fc, Gc, d, km)
-
+% ComputeMotorForces: Computes the feedback rotor forces based on the
+% distance between each motor and the center of mass and the control moment
+% coefficient
+%
+% Inputs:
+%       Fc: Control Force Vector
+%       Mc: Control Moment Vector
+%       d: Distance from center of each rotor to quadcopter center of mass
+%       km: Control Moment Coefficient
 
     ControlVec = [Fc(3),Gc(1),Gc(2),Gc(3)];
-
+    
+    % rotation matrix for rotor force feedback control
     A_matrix = [-1,-1,-1,-1;
         -d/sqrt(2),-d/sqrt(2),d/sqrt(2),d/sqrt(2);
         d/sqrt(2),-d/sqrt(2),-d/sqrt(2),d/sqrt(2);
--- RotationDerivativeFeedback.m
+++ RotationDerivativeFeedback.m
@@ -1,20 +1,33 @@
 function [Fc, Gc] = RotationDerivativeFeedback(var, m, g)
+% RotationDerivativeFeedback: Calculates the control vectors Fc and Gc.
+%
+% Inputs: 
+%       var: 12x1 aircraft state vector
+%       m: mass of quadcopter
+%       g: acceleration due to gravity
+%
+% Outputs:
+%       Fc: Control Force Vector
+%       Gc: Control Moment Vector
 
-x = var(1,:);
-p = var(10,:);
-q = var(11,:);
-r = var(12,:);
 
+x = var(1);
+p = var(10);
+q = var(11);
+r = var(12);
 
-Fc = zeros(length(x),3);
-Gc = zeros(length(x),3);
+Fc = [0;0;0];
+Gc = [0;0;0];
 
+% Control force proportional to weight
+Fc(3) = m*g;
 
-for i = 0:length(x)
-    Fc(i,3) = m*g;
-    Gc(i,1) = -0.004*p(i);
-    Gc(i,2) = -0.004*q(i);
-    Gc(i,3) = -0.004*r(i);
-end
+% Control moments about each body axis proportional to the rotational rates 
+% about their respective axes, but in the opposite sign of the angular 
+% velocity with a gain of 0.004 Nm/(rad/sec)
+Gc(1) = -0.004*p;
+Gc(2) = -0.004*q;
+Gc(3) = -0.004*r;
+
 end
 
